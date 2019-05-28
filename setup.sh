d1c1738960efed735fc618062a1901481f41bcd0
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2019-05-28 23:25:33 +0200

    add config

---
+++ setup.sh
@@ -0,0 +1,17 @@
+﻿#!/bin/bash -e
+
+export PATH=/opt/default-hadoop/bin:/opt/default-spark/bin/:$PATH
+export HADOOP_HOME=/opt/default-hadoop
+export HADOOP_CONF_DIR=/home/maik/workspace/code-admin-clusters/betaweb/hadoop/betaweb
+export SPARK_CONF_DIR="/home/maik/workspace/aitools4-aq-cluster-computing/conf/spark"
+export SPARK_LOCAL_IP="10.141.0.14"
+export HADOOP_USER_NAME=kibi9872
+
+
+hdfs dfs -ls /user/${HADOOP_USER_NAME}
+
+#hadoop jar /opt/default-hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.7.1.jar pi 1000 1000
+
+#mvn clean install
+
+#spark-submit --class de.webis.spark_cw09_tutorial.App target/spark-cw09-tutorial-1.0-SNAPSHOT.jar
--- src/main/java/de/webis/spark_cw09_tutorial/App.java
+++ src/main/java/de/webis/spark_cw09_tutorial/App.java
@@ -1,5 +1,6 @@
 package de.webis.spark_cw09_tutorial;
 
+import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
 import org.apache.spark.api.java.JavaSparkContext;
 
@@ -11,9 +12,18 @@ public class App
 {
     public static void main( String[] args )
     {
-    	try(JavaSparkContext context = new JavaSparkContext(new SparkContext()))
+    	try(JavaSparkContext context = new JavaSparkContext(context()))
     	{
         	System.out.println(context.textFile("bla").aggregate(0, null, null));	
     	}
+    }
+    
+    private static SparkContext context()
+    {
+    	SparkConf conf = new SparkConf(true);
+    	conf.setAppName("maik-froebe-hello-world-spark");
+    	SparkContext ret = new SparkContext(conf);
+    	
+    	return ret;
     }
 }
