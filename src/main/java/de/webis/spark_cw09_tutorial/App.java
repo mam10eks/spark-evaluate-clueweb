4489b67e5ba21e8d9d8bfb2ab7a39b611fea5631
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2019-05-28 22:14:46 +0200

    Initial commit

---
+++ src/main/java/de/webis/spark_cw09_tutorial/App.java
@@ -0,0 +1,13 @@
+package de.webis.spark_cw09_tutorial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
